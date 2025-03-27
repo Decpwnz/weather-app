038b54638136fb323dea55189fe4ca244d450124
Marijus <electromenn@gmail.com>
2025-03-27 15:52:28 +0200

    fix: remove unused ThemeProvider

--- frontend/src/test/test-utils.tsx
+++ frontend/src/test/test-utils.tsx
@@ -1,9 +1,7 @@
 import { ReactElement } from 'react'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
-import { ThemeProvider } from '@mui/material/styles'
 import { configureStore } from '@reduxjs/toolkit'
-import { theme } from '../styles/theme'
 import weatherReducer from '../store/slices/weatherSlice'
 
 function renderWithProviders(
@@ -18,11 +16,7 @@ function renderWithProviders(
   } = {}
 ) {
   function Wrapper({ children }: { children: React.ReactNode }) {
-    return (
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </Provider>
-    )
+    return <Provider store={store}>{children}</Provider>
   }
 
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
